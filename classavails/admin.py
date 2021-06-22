d316af112765f2244c4fb6823f11a6fcc3d6499e
Alyssia Chen <alyssiachen@MacBook-Air-Retina.local>
2021-06-21 22:54:01 -1000

    Added fields to classAvails model

--- classavails/admin.py
+++ classavails/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ClassAvails
 
 # Register your models here.
+
+admin.site.register(ClassAvails)
--- classavails/models.py
+++ classavails/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+from django.views.generic.detail import SingleObjectTemplateResponseMixin
 
 # Create your models here.
 
 class ClassAvails(models.Model):
     CRN = models.PositiveIntegerField()
-    department = models.CharField(max_length=200)
-    courseNumber = models.PositiveIntegerField()
+    course = models.CharField(max_length=10, default='ABC 000')
+    section = models.IntegerField(default = -1)
+    title = models.CharField(max_length=100, default = 'XXXXXX')
+    credits = models.CharField(max_length=25, default='-1')
+    instructor = models.CharField(max_length=50, default = 'XXXXXXXXX')
+    curEnrolled = models.IntegerField(default = -1)
+    seatsAvail = models.IntegerField(default = -1)
+    curWaitlisted = models.IntegerField(default = -1)
+    waitlistAvailable = models.IntegerField(default = -1)
+    days = models.CharField(max_length=10, default = -1)
+    time = models.CharField(max_length=100, default = 'XX-XX')
+    location = models.CharField(max_length=50, default = 'XXX')
+    dates = models.CharField(max_length=50, default = 'XXX')
+
+    def __str__(self):
+        # LATER Add a tostring representation of CRN?
+        return self.title
